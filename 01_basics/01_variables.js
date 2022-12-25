98b65d108943e1ab657bf122df1da0026af82310
Tanishq Anand <tanishq0087@gmail.com>
2022-12-25 16:48:26 +0000

    update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
 const accountId = 123; // Its value cannot be changed
 let accountEmail = "tanishq@google.com";
-var accountPassword = "12345"; // Use let intead of var because of the block scope and functional scope problem
+var accountPassword = "12345"; // Use let intead of var because of the block scope and functional scope problem with var
 accountCity = "Jaipur"; // Not Recommended
 
 let accountState;
