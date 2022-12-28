2ee3b4fbe248a82d7c0e3d8485f414cb6a1cfb15
Tanishq Anand <tanishq0087@gmail.com>
2022-12-28 10:41:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); Error: Because we are using nodejs not browser
+
+console.log(3
+    +
+    3)  // Code readability should be high
+
+let name = "Tanishq"
+let age = 25            
+let isLoggedIn = true
+
+/*
+Primitive:
+    number => 2^53
+    bigint
+    string => ""
+    boolean => true/false
+    null => datatype as well as standalone value (representation of empty value)
+    undefined => value has not be assigned
+    symbol => find uniqueness
+
+Object:
+    typeof => console.log(typeof null) -> object
+              console.log(typeof undefined) -> undefined
+
+*/
